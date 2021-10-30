ac36e56c75aeb40879d599de900b568d22ec34db
Arthur Scarpati <arthur_159@hotmail.com>
2021-10-30 15:32:00 -0300

    List All

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,7 @@
 package one.digitalinnovation.personapi.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +29,7 @@ public class PersonController {
 		return personService.createPerson(personDto);
 	}
 	
+	public List<PersonDTO> listAll(){
+		return personService.listAll();
+	}
 }
